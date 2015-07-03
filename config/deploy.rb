2800e36cb8df412f539ebd4e9cc94fcd32be40e4
Martin Fernandez <me@bilby91.com>
2015-07-03 03:56:23 -0300

    Add unicorn tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,3 +37,30 @@ set :symlinks, [
 
 # Nginx upstream
 set :app_server_socket, "#{shared_path}/tmp/sockets/unicorn.sock"
+
+set :unicorn_conf, "#{current_path}/unicorn.rb"
+set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
+
+namespace :deploy do
+
+  task :restart do
+    on roles(:app) do
+      execute "if [ -f #{fetch(:unicorn_pid)} ]; then kill -USR2 `cat #{fetch(:unicorn_pid)}`; else cd #{current_path} && bundle exec unicorn -c #{fetch(:unicorn_conf)} -D; fi"
+    end
+  end
+
+  task :start do
+    on roles(:app) do
+      within current_path do
+        execute :bundle, "exec unicorn", "-c", fetch(:unicorn_conf), "-D"
+      end
+    end
+  end
+
+  task :stop do
+    on roles(:app) do
+      execute "if [ -f #{fetch(:unicorn_pid)} ]; then kill -QUIT `cat #{fetch(:unicorn_pid)}`; fi"
+    end
+  end
+
+end
--- unicorn.rb
+++ unicorn.rb
@@ -1,9 +1,12 @@
+require 'dotenv'
+Dotenv.load
+
 deploy_path = ENV['DEPLOY_PATH']
 
 timeout 30
 worker_processes 1
-listen "#{deploy_path}/shared/tmp/sockets/unicorn.sock", backlog: 1024
+listen "#{deploy_path}/current/tmp/sockets/unicorn.sock", backlog: 1024
 
-pid "#{deploy_path}/shared/tmp/pids/unicorn.pid"
-stderr_path "#{deploy_path}/shared/log/error.log"
-stdout_path "#{deploy_path}/shared/log/production.log"
+pid "#{deploy_path}/current/tmp/pids/unicorn.pid"
+stderr_path "#{deploy_path}/current/log/error.log"
+stdout_path "#{deploy_path}/current/log/production.log"
